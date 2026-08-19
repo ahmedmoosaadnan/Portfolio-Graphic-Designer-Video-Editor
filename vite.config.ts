e9d5de634ec542b8e7bee3473d674d51f2637d31
Ahmed Moosa Adnan <Moosaalvi963@gmail.com>
2026-08-19 17:15:33 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-graphic-designer-video-editor/',
+  base: '/Portfolio-Graphic-Designer-Video-Editor/', // ⚠️ P, G, D, V, E ko capital karein jaisa repo ka naam hai
 });
